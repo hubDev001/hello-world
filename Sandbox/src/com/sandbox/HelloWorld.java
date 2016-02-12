7f09fa0379aefe437abeb84dff0cde1492ce6ca9
hubDev001 <dan.mortimer@sainsburys.co.uk>
2016-02-12 11:01:35 +0000

    HelloWorld.java - adding class comments

--- Sandbox/src/com/sandbox/HelloWorld.java
+++ Sandbox/src/com/sandbox/HelloWorld.java
@@ -1,5 +1,11 @@
 package com.sandbox;
 
+
+/**
+ * A class, which is an example of the classic "HelloWorld"
+ * 
+ * @author Dan.Mortimer
+ */
 public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello World");
